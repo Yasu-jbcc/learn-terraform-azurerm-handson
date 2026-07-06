16c02d73d00aa7f974194710ac98c38f4e0f5b56
zmd92918-oss <mengdan.zhao@hashicorp.com>
2026-07-06 17:36:38 +0900

    add resources

--- main.tf
+++ main.tf
@@ -6,3 +6,34 @@ resource "azurerm_resource_group" "main" {
     "Author"      = "mandy"
   }
 }
+
+# virtual network
+resource "azurerm_virtual_network" "demo" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+}
+
+# Subnet
+resource "azurerm_subnet" "demo" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.demo.name
+  virtual_network_name = azurerm_virtual_network.demo.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+# Storage Account
+resource "azurerm_storage_account" "demo" {
+  name                     = "${var.storage_account_prefix}${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,33 @@ variable "resource_group_name" {
 variable "location" {
   type = string
 }
+
+variable "vnet_name" {
+  description = "vnet名称"
+  type        = string
+  default     = "vnet-handson-demo"
+}
+
+variable "vnet_address_space" {
+  description = "vnetアドレススペース"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "subnet名称"
+  type        = string
+  default     = "subnet-handson-demo"
+}
+
+variable "subnet_address_prefix" {
+  description = "subnetアドレスプレフィックス"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "storage_account_prefix" {
+  description = "storage account名称のプレフィックス（グローバルでユニークである必要があります）"
+  type        = string
+  default     = "sthandsondemo"
+}
